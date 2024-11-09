6d31aaae33badc6aba0ce880b5d5e90187cc6742
Mrunal Dilip Biranwar <157902716+MrunalBiranwar@users.noreply.github.com>
2024-11-09 17:33:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Joke from "./components/joke";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joke Generator Using React and Joke API</h1>
+      <Joke/>
+    </div>
+  );
+}
+
+export default App;
